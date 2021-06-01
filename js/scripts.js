1832dd5eb546dbca22cbe98458c656db6499ba23
hannesdonel <mail@hannesdonel.de>
2021-06-01 23:46:10 +0200

    Add Loop to display Pokémons in DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//This is the definition of the Pokémon List
+
 function Pokemon(name, height, types) {
   this.name = name;
   this.height = height;
@@ -9,3 +11,14 @@ let pokemonList = [
   new Pokemon("Togepi", 0.3, ["fairy"]),
   new Pokemon("Poliwhirl", 1, ["water"]),
 ];
+
+//List the Pokémons in DOM
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(
+      "<br>" + pokemonList[i].name + " (height: " + pokemonList[i].height + " m)"
+  );
+  if (pokemonList[i].height > 1) {
+    document.write(" - Wow, that's big!");
+  }
+}
