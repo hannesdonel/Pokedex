4ac6b9c3a3bf35a4450482a007a2ac7df197fda9
hannesdonel <mail@hannesdonel.de>
2021-06-22 13:19:39 +0200

    Improvements
    - fix Header in front of Main on small screens
    - add all animations to styles.css
    - fix all ESLint errors on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 // When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar.
 
-let prevScrollpos = window.pageY;
 window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  if ((prevScrollpos = currentScrollPos)) {
-    document.getElementById("hide-header").style.top = "-100vh";
-  } else {
+  const scrolled = window.scrollY;
+  if ((scrolled === 0)) {
     document.getElementById("hide-header").style.top = "0";
+  } else {
+    document.getElementById("hide-header").style.top = "-100vh";
   }
 };
 
 // Definition of the Pokémon Repository
 
-let pokemonRepository = (function () {
-  let pokemonList = [];
-  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
-  let popupContainer = document.querySelector("#popup-container");
+const pokemonRepository = (function () {
+  const pokemonList = [];
+  const apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+  const popupContainer = document.querySelector("#popup-container");
 
   // Add a Pokémon
 
@@ -23,11 +22,11 @@ let pokemonRepository = (function () {
     if (typeof pokemon !== "object") {
       alert("The new Pokémon should have the form of an object");
     } else if (
-      JSON.stringify(Object.keys(pokemon)) !==
-      JSON.stringify(["name", "detailsUrl"])
+      JSON.stringify(Object.keys(pokemon))
+      !== JSON.stringify(["name", "detailsUrl"])
     ) {
       alert(
-        "The object should have the following properties: name, detailsUrl"
+        "The object should have the following properties: name, detailsUrl",
       );
     } else pokemonList.push(pokemon);
   }
@@ -41,15 +40,15 @@ let pokemonRepository = (function () {
   // Create a button in the for each Pokémon within pokemonList
 
   function addListItem(Pokemon) {
-    let $list = document.querySelector(".pokemon-list");
-    let listItem = document.createElement("li");
-    let button = document.createElement("button");
+    const $list = document.querySelector(".pokemon-list");
+    const listItem = document.createElement("li");
+    const button = document.createElement("button");
     listItem.classList.add("pokemon-list__item");
     button.innerText = Pokemon.name;
     button.classList.add("pokemon-list__item--button");
     listItem.appendChild(button);
     $list.appendChild(listItem);
-    button.addEventListener("click", function () {
+    button.addEventListener("click", () => {
       showDetails(Pokemon);
     });
   }
@@ -59,60 +58,57 @@ let pokemonRepository = (function () {
   function loadList() {
     showLoader();
     return fetch(apiUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (json) {
-        for (item of json.results) {
-          let pokemon = {
+      .then((response) => response.json())
+      .then((json) => {
+        json.results.forEach((item) => {
+          const pokemon = {
             name: item.name,
             detailsUrl: item.url,
           };
           add(pokemon);
-        }
+        });
       })
-      .catch(function (e) {
+      .catch((e) => {
         console.error(e);
       })
       .finally((e) => {
-        hideLoader();
+        hideLoader(e);
       });
   }
 
   // Get Pokémon details from API
 
-  function loadDetails(item) {
-    let url = item.detailsUrl;
+  function loadDetails(itemDetails) {
+    const url = itemDetails.detailsUrl;
+    const pokemon = itemDetails;
     showLoader();
     return fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (details) {
-        item.imageUrl = details.sprites.other.dream_world.front_default;
-        item.abilities = [];
-        for (i = 0; i < details.abilities.length; i++) {
-          item.abilities.push(` ${details.abilities[i].ability.name}`);
+      .then((response) => response.json())
+      .then((details) => {
+        pokemon.imageUrl = details.sprites.other.dream_world.front_default;
+        pokemon.abilities = [];
+        for (let i = 0; i < details.abilities.length; i++) {
+          pokemon.abilities.push(` ${details.abilities[i].ability.name}`);
         }
-        item.height = details.height;
-        item.types = [];
-        for (i = 0; i < details.types.length; i++) {
-          item.types.push(` ${details.types[i].type.name}`);
+        pokemon.height = details.height;
+        pokemon.types = [];
+        for (let i = 0; i < details.types.length; i++) {
+          pokemon.types.push(` ${details.types[i].type.name}`);
         }
-        item.weight = details.weight;
+        pokemon.weight = details.weight;
       })
-      .catch(function (e) {
+      .catch((e) => {
         console.error(e);
       })
       .finally((e) => {
-        hideLoader();
+        hideLoader(e);
       });
   }
 
   // Show details of Pokémon
 
   function showDetails(item) {
-    loadDetails(item).then(function () {
+    loadDetails(item).then(() => {
       showPopup(item);
     });
   }
@@ -123,13 +119,13 @@ let pokemonRepository = (function () {
     popupContainer.innerHTML = "";
     popupContainer.classList.add("fadeIn");
 
-    let popup = document.createElement("div");
-    let closeButton = document.createElement("button");
-    let popupImage = document.createElement("img");
-    let popupHeading = document.createElement("h1");
-    let popupContent = document.createElement("p");
+    const popup = document.createElement("div");
+    const closeButton = document.createElement("button");
+    const popupImage = document.createElement("img");
+    const popupHeading = document.createElement("h1");
+    const popupContent = document.createElement("p");
     popup.classList.add("popup", "zoomIn");
-    closeButton.innerText = "X";
+    closeButton.innerHTML = "&times";
     closeButton.style.fontSize = "24px";
     popupImage.src = item.imageUrl;
     popupImage.alt = "Picture of selected Pokémon";
@@ -137,8 +133,8 @@ let pokemonRepository = (function () {
     popupImage.height = 300;
     popupHeading.innerText = item.name;
     popupContent.innerHTML = `
-      <p class="block-level"><span class="capitalize"><b>Abilities: </b>${item.abilities}</p>
-      <p class="block-level"><b>Types: </b>${item.types}</span></p>
+      <p class="block-level capitalize"><b>Abilities: </b>${item.abilities}</p>
+      <p class="block-level capitalize"><b>Types: </b>${item.types}</p>
       <p class="block-level"><b>Height: </b>${item.height}0 cm</p>
       <p class="block-level"><b>Weight: </b>${item.weight}00 g</p>
       `;
@@ -155,14 +151,14 @@ let pokemonRepository = (function () {
     closeButton.addEventListener("click", hidePopup);
     window.addEventListener("keydown", (e) => {
       if (
-        e.key === "Escape" &&
-        popupContainer.classList.contains("is-visible")
+        e.key === "Escape"
+        && popupContainer.classList.contains("is-visible")
       ) {
         hidePopup();
       }
     });
     window.addEventListener("click", (e) => {
-      let target = e.target;
+      const { target } = e;
       if (target === popupContainer) {
         hidePopup();
       }
@@ -172,7 +168,7 @@ let pokemonRepository = (function () {
   // Hide popup
 
   function hidePopup() {
-    let popup = document.querySelector(".popup");
+    const popup = document.querySelector(".popup");
     popupContainer.classList.add("fadeOut");
     popupContainer.classList.remove("fadeIn");
     popup.classList.remove("zoomIn");
@@ -185,29 +181,29 @@ let pokemonRepository = (function () {
   // Loader animations
 
   function showLoader() {
-    let loader = document.querySelector("body");
+    const loader = document.querySelector("body");
     loader.style.cursor = "progress";
   }
 
   function hideLoader() {
-    let loader = document.querySelector("body");
+    const loader = document.querySelector("body");
     loader.style.cursor = "default";
   }
 
   return {
-    add: add,
-    getAll: getAll,
-    addListItem: addListItem,
-    loadList: loadList,
-    loadDetails: loadDetails,
-    showDetails: showDetails,
+    add,
+    getAll,
+    addListItem,
+    loadList,
+    loadDetails,
+    showDetails,
   };
-})();
+}());
 
 // List the Pokémon Buttons in DOM
 
-pokemonRepository.loadList().then(function () {
-  pokemonRepository.getAll().forEach(function (pokemon) {
+pokemonRepository.loadList().then(() => {
+  pokemonRepository.getAll().forEach((pokemon) => {
     pokemonRepository.addListItem(pokemon);
   });
 });
